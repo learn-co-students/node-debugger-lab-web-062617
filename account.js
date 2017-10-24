dc595a43f6b2ef9c65ca873b600606803faab03c
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-10-24 14:39:56 -0400

    Done.

--- account.js
+++ account.js
@@ -7,7 +7,7 @@ module.exports = function account() {
   }
 
   function setAccountType () {
-    if (customerAccount.checking === 1) {
+    if (customerAccount.checking == 1) {
       customerAccount.type = 'checking'
     } else {
       customerAccount.type = 'savings'
@@ -16,16 +16,15 @@ module.exports = function account() {
 
   var signupBonus = 250,
     deposit = 1000
-
   var openAccount = function(account, deposit) {
     account.balance += signupBonus
     output['New balance after signup bonus is '] = account.balance
     account.balance += deposit
   }
 
-  output['Account before opening'] = 'customerAccount'
+  output['Account before opening'] = customerAccount
 
-  openAccount(customerAccount)
+  openAccount(customerAccount, deposit)
   setAccountType()
   output['Account after opening'] = customerAccount
   return output
